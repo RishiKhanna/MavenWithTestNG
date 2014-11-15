07f094f7415e37370a8fd8850930378bdad7d9fc
Rishi Khanna <rishi.khanna07@gmail.com>
2014-11-15 20:47:21 +0530

    v 5.1
    removing locator files

--- src/main/java/com/generic/web/pages/locators/ConfirmationPageLocators.java
+++
@@ -1,8 +0,0 @@
-package com.generic.web.pages.locators;
-
-import org.openqa.selenium.By;
-
-public interface ConfirmationPageLocators {
-
-	By confirmMessage = By.cssSelector("#perfect>tbody>tr>td>font");
-}
